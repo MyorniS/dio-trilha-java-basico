122991686e1938b43b38cd32762f273bf6aca28c
muril <muril@DESKTOP-SUR4LJ1>
2024-02-08 07:46:54 -0300

    Condicional Composta

--- ControleFluxo/src/CaixaEletronico.java
+++ ControleFluxo/src/CaixaEletronico.java
@@ -1,16 +1,26 @@
+import java.util.Scanner;
 
 public class CaixaEletronico {
 
 	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
 		
-		//Condicional Simples
+		//Condicional Composta
 		double saldo = 25.0;
-		double valorSolicitado = 17.0;
 
-		if(valorSolicitado < saldo) 
-			saldo = saldo - valorSolicitado;
+		System.out.println("Sua conta tem o saldo de: " + saldo);
+		System.out.println("Solicite um valor para retirada: "); 
+		double valorSolicitado = ler.nextDouble();
 		
-		System.out.println(saldo);
+		if(valorSolicitado <= saldo) {
+			saldo = saldo - valorSolicitado;
+			System.out.println("Os R$" + valorSolicitado + " que foi solicitado podem ser retirados");
+			System.out.println("Saldo atual: " + saldo);
+		} 
+		else
+			System.out.println("Saldo insuficiente");
+		
+		
 		
 	}
 
