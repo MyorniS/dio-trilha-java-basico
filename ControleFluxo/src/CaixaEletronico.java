a52ee43045e8fdf1b77b57ca792eeb71e8fc286b
muril <muril@DESKTOP-SUR4LJ1>
2024-02-08 08:16:27 -0300

    Testes com Condicionais

--- ControleFluxo/src/CaixaEletronico.java
+++ ControleFluxo/src/CaixaEletronico.java
@@ -9,16 +9,27 @@ public class CaixaEletronico {
 		double saldo = 25.0;
 
 		System.out.println("Sua conta tem o saldo de: " + saldo);
-		System.out.println("Solicite um valor para retirada: "); 
-		double valorSolicitado = ler.nextDouble();
+		System.out.println("Deseja retirar um valor? Digite 1 para Sim e 0 para Não");
+		int resp = ler.nextInt();
 		
-		if(valorSolicitado <= saldo) {
-			saldo = saldo - valorSolicitado;
-			System.out.println("Os R$" + valorSolicitado + " que foi solicitado podem ser retirados");
-			System.out.println("Saldo atual: " + saldo);
-		} 
-		else
-			System.out.println("Saldo insuficiente");
+		if(resp > 0) {
+			System.out.println("Informe o valor para retirada: "); 
+			double valorSolicitado = ler.nextDouble();
+			if(valorSolicitado <= saldo) {
+				saldo = saldo - valorSolicitado;
+				System.out.println("Os R$" + valorSolicitado + " que foi solicitado podem ser retirados");
+				System.out.println("Saldo atual: " + saldo);
+			} 
+			else
+				System.out.println("Saldo insuficiente");
+					
+		}else
+			System.out.println("Ok, então até outro dia :)");
+		
+		
+		
+		
+		
 		
 		
 		
