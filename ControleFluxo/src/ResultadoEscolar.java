a89b238c10cd32c89f968e08e1ecf5357cc47d93
muril <muril@DESKTOP-SUR4LJ1>
2024-02-08 12:55:34 -0300

    Condicional ternária

--- ControleFluxo/src/ResultadoEscolar.java
+++ ControleFluxo/src/ResultadoEscolar.java
@@ -3,17 +3,22 @@ public class ResultadoEscolar {
 
 	public static void main(String[] args) {
 		
-		//Condicional encadeada
+		//Condicional ternária
 
 		int nota = 10;
 		
 		System.out.println("Nota: " + nota);
-		if(nota >= 7)
-			System.out.println("Aprovado :)");
-		else if(nota >=5 && nota < 7)
-			System.out.println("Prova Recuperação :/");
-		else
-			System.out.println("Reprovado :(");
+		
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
+		
+//		Condicional encadeada
+//		if(nota >= 7)
+//			System.out.println("Aprovado :)");
+//		else if(nota >=5 && nota < 7)
+//			System.out.println("Prova Recuperação :/");
+//		else
+//			System.out.println("Reprovado :(");
 		
 	}
 
